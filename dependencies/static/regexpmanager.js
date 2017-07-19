949fdd3e9fb71f440b908a2d403a8f1383078c5f
Bogdan Didenko <bogdan@webspellchecker.net>
2017-07-19 16:14:03 +0300

    add regexpmanager tests. finish regexpmanager functionality

--- dependencies/static/regexpmanager.js
+++ dependencies/static/regexpmanager.js
@@ -5,46 +5,79 @@
 (function(){
     'use strict';
 	function init( Namespace ) {
+        function removeSetBrackets(string) {
+            for(var i = 0; i < string.length; i += 1) {
+                if(string[i - 1] !== '\\' && (string[i] === '[' || string[i] === ']')) {
+                    string = string.substring(0, i) + string.substring(i + 1);
+                }
+            }
 
-         function RegularType(name, sourse, flags) {
+            return string;
+        }
+
+        var Regularssources = {
+            space: "[\\s\\xA0]",
+            dot: '\\.',
+            digits : '[0-9]',
+            //Punctuations
+            HtmlSpaceSymbol:  String.fromCharCode(160),
+            // \u061F - Arabic question mark, \u060C - Arabic comma, \u061B - Arabic semicolon
+            textPunctuation: ",\"\^\$\*\+\?\=\!\:\|\\\/\(\)\[\]\{\}\>\<\@\%\&\#\;\_\~\u061F\u060C\u061B\u0001-\u001F\u0080-\u00B6\u00B8-\u00BF\u2000-\u200A\u200C-\u266F\u2E00-\u2E7F",
+            situationalSeparators: "\\.\\-\\'",
+            EOL: '[\r\n(\r\n)]',
+            specialCharacters: String.fromCharCode(8203) + String.fromCharCode(65279),            
+            ip: '/^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))*$/',
+            twoAndMore: '{2,}'
+        };
+
+        function RegularType(name, source, flags) {
             this.name = name;
             this.flags = flags || '';
-            this.sourse = sourse;
+            this.source = source;
             this.string = '';
-            var init = function(string) {
-                return this.setString(string);
-            };
-            return init.bind(this);
         }
 
         RegularType.prototype = {
             _wrapSymbol: '#',
             _getRegExp: function() {
-                 return new RegExp(this.sourse, this.flags);
+                 return new RegExp(this.source, this.flags);
             },
             _combain: function(options) {
+                var regExpObject = options.regExpObject;
                 var separator = options.separator || '';
                 var nameConcatinator = options.nameConcatinator || '&';
-                var sourse = this.sourse + separator + options.sourse;
-                var name = options.compositionName || (this.name + options.nameConcatinator + options.name);
-                var combainRegType = new RegularType(name, sourse)(this.string);
-                combainRegType.addFlags(options.flags + this.flags);
+                var source = this.source + separator + regExpObject.source;
+                var name = options.compositionName || (this.name + options.nameConcatinator + regExpObject.name);
+                var combainRegType = new RegularType(name, source).init(this.string);
+                combainRegType = combainRegType.addFlags(regExpObject.flags + this.flags);
                 return combainRegType;
             },
-            _setString: function(string) {
+            _replaceSource: function(subStr, source) {
+                return this._updateSource(source).replace(subStr);
+            },
+            _updateSource: function(source) {
+                var clone = this.clone();
+                clone.source = source;
+                return clone;
+            },
+            init: function(string) {
+                return this.setString(string);
+            },
+            clone: function() {
+                var clone = Object.create(RegularType.prototype);
+                return Object.assign(clone, this) ;
+            },
+            setString: function(string) {
                 var clone = this.clone();
                 clone.string = string;
                 return clone;
             },
-            clone: function() {
-                return Object.assign({}, this);
-            },
-            getSring: function() {
+            getString: function() {
                 return this.string;
             },
             addFlags: function(flags) {
                 var clone = this.clone(),
-                    flagsArr = flags.split();
+                    flagsArr = flags.split('');
                 flagsArr.forEach(function(flag) {
                     if( clone.flags.indexOf(flag) === -1 ) {
                         clone.flags += flag;
@@ -53,81 +86,62 @@
                 return clone;
             },
             g: function() {
-                return this.clone().addFlags('g');
+                return this.addFlags('g');
             },
             i: function() {
-                return this.clone().addFlags('i');
+                return this.addFlags('i');
             },
             set: function() {
-                var clone = this.clone();
-                clone.sourse = '[' + clone.sourse + ']';
-                return clone;
+                var source = removeSetBrackets(this.source);
+                return this._updateSource('[' + source + ']');
+            },
+            group: function() {
+                return this._updateSource('(' + this.source + ')');
             },
             start: function() {
-                var clone = this.clone();
-                clone.sourse = '^' + clone.sourse;
-                return clone;
+                return this._updateSource('^' + this.source );
             },
             end: function() {
-                var clone = this.clone();
-                clone.sourse += '$';
-                return clone;
+                return this._updateSource(this.source + '$');
             },
             split: function() {
                 return this.string.split( this._getRegExp() );
             },
             replace: function(subStr) {
-                this.string.replace(this._getRegExp(), subStr);
-                return this;
-            },
-            replaceWrapped: function(subStr, wrapSymbol) {
-                wrapSymbol = wrapSymbol || this._wrapSymbol;
                 var clone = this.clone();
-                clone.sourse = wrapSymbol + clone.sourse + wrapSymbol;
-                clone.replace(clone.string, subStr);
+                clone.string = clone.string.replace(this._getRegExp(), subStr);
                 return clone;
             },
             replaceLeftWrapped: function(wrapSymbol, subStr) {
                 wrapSymbol = wrapSymbol || this._wrapSymbol;
-                var clone = this.clone();
-                clone.sourse = wrapSymbol + clone.sourse;
-                clone.replace(clone.string, subStr);
-                return clone;
+                return this._replaceSource(subStr, wrapSymbol + this.source);
             },
             replaceRightWrapped: function(wrapSymbol, subStr) {
                 wrapSymbol = wrapSymbol || this._wrapSymbol;
-                var clone = this.clone();
-                clone.sourse = clone.sourse + wrapSymbol;
-                clone.replace(clone.string, subStr);
-                return clone;
+                return this._replaceSource(subStr, this.source + wrapSymbol);
             },
             wrapInclude: function(wrapSymbol) {
                 wrapSymbol = wrapSymbol || this._wrapSymbol;
-                this.replace(this.string, wrapSymbol+"$1"+wrapSymbol);
-                return this;
+                return this.group().replace(wrapSymbol+"$1"+wrapSymbol);
             },
             or: function(regExpObject, compositionName) {
                 return this._combain({
                     compositionName: compositionName,
-                    flags: regExpObject.flags,
-                    sourse: regExpObject.sourse,
+                    regExpObject: regExpObject,
                     separator: '|',
                     nameConcatinator: '|'
                 });
             },
             composition: function(regExpObject, compositionName) {
-                var flags = regExpObject.flags + this.flags,
-                    name = compositionName || this.name + '&' + regExpObject.name,
-                    sourse = regExpObject.sourse + this.sourse;
-                
                 return this._combain({
-                    compositionName: compositionName,
-                    flags: regExpObject.flags,
-                    sourse: regExpObject.sourse
+                    regExpObject: regExpObject,
+                    separator: '',
+                    nameConcatinator: '&',
+                    compositionName: compositionName
                 });
             },
             compositionGraph: function(listOfRegExpObjects, compositionName) {
-                var compositionObj;
+                var compositionObj = (this instanceof RegularType) ? this : undefined;
                 listOfRegExpObjects.forEach(function(element) {
                     compositionObj = (compositionObj) ? compositionObj.composition(element) : element;
                 }, this);
@@ -136,25 +150,12 @@
             }
         };
 
-        var RegularsSourses = {
-            space: "xA0\s",
-            dot: '\.',
-            digits : '[0-9]',
-            //Punctuations
-            HtmlSpaceSymbol:  String.fromCharCode(160),
-            // \u061F - Arabic question mark, \u060C - Arabic comma, \u061B - Arabic semicolon
-            textPunctuation: ",\^\$\*\+\?\=\!\:\|\\\/\(\)\[\]\{\}\>\<\@\%\&\#\;\_\~\u061F\u060C\u061B\u0001-\u001F\u0080-\u00B6\u00B8-\u00BF\u2000-\u200A\u200C-\u266F\u2E00-\u2E7F",
-            situationalSeparators: "\.\-\'",
-            EOL: '\r\n(\r\n)\"',
-            specialCharacters: String.fromCharCode(8203) + String.fromCharCode(65279),            
-            ip: '/^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))*$/',
-            twoAndMore: '{2,}'
-        };
+     
 
         var RegularsManager = {};
         
-        for(var k in RegularsSourses){
-            RegularsManager[k] = new RegularType(k, RegularsSourses[k]);
+        for(var k in Regularssources){
+            RegularsManager[k] = new RegularType(k, Regularssources[k]);
         }
         
         Namespace.RegularsManager = RegularsManager;
--- tests/regexpmanager-spec.js
+++ tests/regexpmanager-spec.js
@@ -1,9 +1,14 @@
-var WebApi = require("../"), api, bool, RegularsManager, optionTypes;
+var WebApi = require("../"), api, bool, RegularsManager, optionTypes,
+    regSpace;
+RegularsManager = WebApi.RegularsManager;
+    regSpace = RegularsManager.space.init('text');
 
-describe("TextProcessor", function () {
+   
+describe("RegularsManager", function () {
     
     beforeEach(function() {
-        RegularsManager = WebApi.RegularsManager;
+        regSpace = RegularsManager.space.init('text');
+        //RegularsManager = WebApi.RegularsManager;
     });
 
     it("loaded", function() {
@@ -16,71 +21,145 @@ describe("TextProcessor", function () {
         expect(RegularsManager.digits).toBeDefined();
     });
     
-    it('regular type should be a function what recive text and return clone RegObj with current text', function() {
-    
+    it('regular type should be a object init should return clone RegObj with current text', function() {
+        expect(typeof RegularsManager.space).toEqual('object');
+        regSpace = RegularsManager.space.init('list');
+        expect(typeof regSpace).toEqual('object');
+        expect(RegularsManager.space).not.toEqual(regSpace);
     });
 
     it('clone method should return regularObject with equal fields', function() {
-
+        var regSpaceClone = regSpace.clone();
+        for (var k in regSpace) {
+            expect(regSpace[k]).toEqual(regSpaceClone[k]);
+        }
     });
 
-    it('getSring method should return preassigned text ', function() {
-
+    it('getString method should return preassigned text ', function() {
+        var text = "standard text";
+        expect( RegularsManager.space.init(text).getString() )
+            .toEqual( text );         
     });
     
     it('addFlags method should add flags to clone of RegObj', function() {
-
+        expect(regSpace.flags).toEqual('');
+        regSpace = regSpace.addFlags('gi');
+        expect(regSpace.flags).toEqual('gi');
+        regSpace = regSpace.addFlags('ig');
+        expect(regSpace.flags).toEqual('gi');
     });
     
     it('g and i methods should add flags', function() {
-
+        expect(regSpace.flags).toEqual('');
+        var _regSpace = regSpace.g().g();
+        expect(_regSpace.flags).toEqual('g');
+        _regSpace = _regSpace.i().i();
+        expect(_regSpace.flags).toEqual('gi');
+        expect(regSpace.i().i().flags).toEqual('i');
     });
     
-    it('set method should wrap regular sourse [ ] square brackets', function() {
-
+    it('set method should wrap regular source [ ] square brackets', function() {
+        var source = regSpace.source;
+        regSpace = regSpace.set().set();
+        expect(regSpace.source).toEqual(source);
+        var regDot = RegularsManager.dot.init('text');
+        source = regDot.source;
+        regDot = regDot.set().set();
+        expect(regDot.source).toEqual('[' + source + ']');
     });
 
-    it('start method should add ^ to start of regular sourse', function() {
-
+    it('group method should wrap regular source ( ) round brackets', function() {
+        var source = regSpace.source;
+        regSpace = regSpace.group();
+        expect(regSpace.source).toEqual('(' + source + ')');
     });
 
-    it('end method should add $ to end of regular sourse', function() {
+    it('start method should add ^ to start of regular source', function() {
+        var source = regSpace.source;
+        regSpace = regSpace.start();
+        expect(regSpace.source).toEqual('^' + source);
+    });
 
+    it('end method should add $ to end of regular source', function() {
+        var source = regSpace.source;
+        regSpace = regSpace.end();
+        expect(regSpace.source).toEqual(source + '$');
     });
 
     it('split method should return array splited by current regExp', function() {
-
+        var resArr = RegularsManager.space.init('test tesst retest').split();
+        expect(resArr.length).toEqual(3);
+        expect(resArr[0], resArr[1], resArr[2]).toEqual('test', 'tesst', 'retest');
     });
 
     it('replace method should replace original text by current regexp and return ', function() {
-
-    });
-
-    it('replaceWrapped method should replace original text wrapped by wrapped symbol', function() {
-
+        var text = RegularsManager.space.init('test tesst retest').replace('').getString();
+        expect(text).toEqual('testtesst retest');
+        text = RegularsManager.space.init('test tesst retest').g().replace('').getString();
+        expect(text).toEqual('testtesstretest');
     });
 
     it('replaceLeftWrapped method should replace text with wrapped symbol in left', function() {
-
+        var text = RegularsManager.space.init('test# retest')
+            .replaceLeftWrapped('#', '<')
+            .getString();
+        expect(text).toEqual('test<retest');
     });
 
     it('replaceRightWrapped method should replace text with wrapped symbol in right', function() {
-
+        var text = RegularsManager.space.init('test #retest')
+            .replaceRightWrapped('#', '>')
+            .getString();
+        expect(text).toEqual('test>retest');
     });
 
     it('wrapInclude method should wrap match in text by wrap symbols', function() {
-
+        var text = RegularsManager.space
+            .init('test retest')
+            .wrapInclude('#')
+            .getString();
+        
+        expect(text).toEqual('test# #retest');
     });
     
     it('composition method should return combain RegObj from original and received', function() {
-
+         var spaceAndDots = RegularsManager.space
+                .init('T .r. r')
+                .composition( RegularsManager.dot )
+                .g(),
+            spaceAndDotsSet = spaceAndDots.set();
+        
+        var text = spaceAndDots.replace('').getString();
+        expect(text).toEqual('Tr. r');
+        text = spaceAndDotsSet.replace('').getString();
+        expect(text).toEqual('Trr');
+        
+        expect(spaceAndDotsSet.source).toEqual('[\\s\\xA0\\.]');
     });
 
     it('or method should return combain RegObj from original and received and separate it by |', function() {
-
+        var situationalSeparatorsSet = RegularsManager.situationalSeparators.set();
+        var text = RegularsManager.space
+            .composition( situationalSeparatorsSet )
+            .group()
+            .or(
+                situationalSeparatorsSet
+                    .composition( RegularsManager.space )
+                    .group()
+            ).g()
+            .init('test-test -res- test- ')
+            .replace('  ')
+            .getString();
+        expect(text).toEqual('test-test  res  test  ');
     });
 
     it('compositionGraph method should return combain RegObj from original and array of received', function() {
-
+        var punctuation = RegularsManager.space.compositionGraph([
+            RegularsManager.dot,
+            RegularsManager.EOL,
+            RegularsManager.digits
+        ]).g().init('test .\r2test .2. ');
+        var text = punctuation.replace('').getString();
+        expect(text).toEqual('testtest .2. ');
     });
 });
