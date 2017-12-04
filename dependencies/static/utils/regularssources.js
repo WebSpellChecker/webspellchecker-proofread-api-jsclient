84743927914a4fd5d92e0e29edbadfca6b4046c3
Bogdan Didenko <bogdan@webspellchecker.net>
2017-12-04 18:10:19 +0200

    Extend regulars whiteSpace and textPunctuation

--- dependencies/static/utils/regularssources.js
+++ dependencies/static/utils/regularssources.js
@@ -8,6 +8,7 @@
 
         var RegularsSources = {
             space: "[\\s\\xA0]",
+            commonSpace: " ",
             nonWhiteSpaceChar: "\\S",
             dot: '\\.',
             anyChar: '.',
@@ -17,13 +18,13 @@
             //Punctuations
             HtmlSpaceSymbol:  String.fromCharCode(160),
             // \u061F - Arabic question mark, \u060C - Arabic comma, \u061B - Arabic semicolon
-            textPunctuation: ",\"\^\$\*\+\?\=\!\:\|\\\/\\(\\)\\[\\]\\{\\}\\>\\<\\@\\%\\&\\#\\;\\_\\~\\u061F\\u060C\\u061B\\u0001-\\u001F\\u0080-\\u00B6\\u00B8-\\u00BF\\u2000-\\u200A\\u200C-\\u266F\\u2E00-\\u2E7F",
+            textPunctuation: " ,\"\^\$\*\+\?\=\!\:\|\\\/\\(\\)\\[\\]\\{\\}\\>\\<\\@\\%\\&\\#\\;\\_\\~\\u061F\\u060C\\u061B\\u0001-\\u001F\\u0080-\\u00B6\\u00B8-\\u00BF\\u2000-\\u200A\\u200C-\\u266F\\u2E00-\\u2E7F",
             situationalSeparators: "\\.\\-\\'",
             sentenceEndChars: "\\.!?",
             specialCharacters: String.fromCharCode(8203) + String.fromCharCode(65279),
             ip: '/^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))*$/',
             twoAndMore: '{2,}',
-            whiteSpace: "\\u00A0\\u1680\\u180E\\u2000-\\u200C\\u2028\\u2029\\u202F\\u205F\\u3000"
+            whiteSpace: " \\f\\n\\r\\t\\v\\uFEFF\\u0085\\u00A0\\u1680\\u180E\\u2000-\\u200C\\u2028\\u2029\\u202F\\u205F\\u3000"
         };
 
         Namespace.RegularsSources = RegularsSources;
