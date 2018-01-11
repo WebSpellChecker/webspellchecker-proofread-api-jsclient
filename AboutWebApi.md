5f9fa0107a1b90089bb2b7a943dcb717ba028bbe
Bogdan Didenko <bogdan@webspellchecker.net>
2018-01-11 11:41:28 +0200

    add documentation build task

---
+++ AboutWebApi.md
@@ -0,0 +1,7 @@
+# WebSpellChecker WebApi
+
+WebSpellChecker is a spell checking solution that specially designed for integration with web-based systems.
+
+---
+
+<#COPYRIGHTS#>
--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,26 @@ module.exports = function(grunt) {
 	source_files = JSON.parse(source_files);
     grunt.initConfig({
         pkg: pkg,
-		source_files: source_files,
+        source_files: source_files,
+        copy: {
+            documentationAboutWebApi: {
+				src: 'AboutWebApi.md',
+				dest: 'tmp/AboutWebApi.md'
+			},
+        },
+        'string-replace': {
+            documentationAboutWebApi: {
+                files: {
+					'tmp/AboutWebApi.md': 'tmp/AboutWebApi.md'
+				},
+				options: {
+					replacements: [{
+						pattern: /(<#COPYRIGHTS#>)/ig,
+						replacement: '<%= full_text_copyrights %>'
+					}]
+				}
+            }
+        },
         concat: {
             webApi: {
                 src: '<%= source_files.webapi %>',
@@ -24,13 +43,43 @@ module.exports = function(grunt) {
                 dest: '<%= pkg.webapi_path %>/dest/webspellchecker-api.js',
                 nonull: true
             }
-        }
+        },
+        jsdoc: {
+            documentationWebApi: {
+				src: ['<%= pkg.webapi_path %>/webapi.js', 'tmp/AboutWebApi.md'],
+				options: {
+					destination : '<%= pkg.webapi_path %>/documentation',
+					configure : "jsdoc.json"
+				}
+			},
+        },
+        clean: {
+			tmp: ['tmp/']
+		}
     });
 
+    // file copy
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	// Load the plugin that provides the "clean" task
+	grunt.loadNpmTasks('grunt-contrib-clean');
     // Load the plugin that provides the "concat" task.
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	// Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+	// replace string
+    grunt.loadNpmTasks('grunt-string-replace');
+
+    grunt.loadNpmTasks('grunt-jsdoc');
+
+    grunt.registerTask('documentation', 'build WebApi documentation', function() {
+        var full_text_copyrights = grunt.option('full_text_copyrights') || grunt.file.read('full_copyrights.txt');
+        grunt.config.data.full_text_copyrights = full_text_copyrights;
+
+        grunt.task.run(['copy:documentationAboutWebApi']);
+        grunt.task.run(['string-replace:documentationAboutWebApi']);
+        grunt.task.run(['jsdoc:documentationWebApi']);
+        grunt.task.run(['clean:tmp']);
+    });
 
 	grunt.registerTask('default', ["concat:webApi", "uglify:webApi"]);
 };
