04e479a1ece51f893e55a1ae139898c3cfd66835
Alexey Neretin <alexey.neretin@webspellchecker.net>
2018-07-30 17:25:50 +0300

    modificationTime field was added to UserDictionary

--- webapi.js
+++ webapi.js
@@ -157,6 +157,7 @@
                     var ud = new UserDictionary({
                             wordlist: responseInfo.wordlist,
                             name: responseInfo.name,
+                            modificationTime: responseInfo.modificationTime,
                             makeUdAction: self._makeUdAction.bind(self)
                         });
 
@@ -581,6 +582,7 @@
         function UserDictionary(parameters) {
             this.name = parameters.name;
             this.wordlist = parameters.wordlist;
+            this.modificationTime = parameters.modificationTime;
             this.makeUdAction = parameters.makeUdAction;
         }
 
