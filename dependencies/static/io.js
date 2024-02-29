b593e896cb251f9b36ea8036bd339b0fc0234959
Alexey Neretin <alexey.neretin@webspellchecker.net>
2024-02-29 14:45:35 +0200

    Fix indentation

--- dependencies/static/io.js
+++ dependencies/static/io.js
@@ -442,7 +442,7 @@
 			 * @param  {Function} onError       Handler unsuccessful response from the server
 			 * @return {String}                 Callback function name
 			 */
-			get: function ( url, withCredentials, onSuccess, onError ) {
+			get: function( url, withCredentials, onSuccess, onError ) {
 				// Make Request using defined request types
 				return IO.request({
 					url: url,
